16ed0f5a323ba701c01627d30e131dad0b0a27cb
wenkai <wenkai.wang@hotmail.com>
2019-02-06 10:41:32 +1030

    initial project

---
+++ src/main/java/springboot/hello/helloSpringboot/HelloSpringbootApplication.java
@@ -0,0 +1,14 @@
+package springboot.hello.helloSpringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringbootApplication.class, args);
+	}
+
+}
+
